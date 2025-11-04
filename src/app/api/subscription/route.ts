fe89969b9586b23b81adf3e8ad9d5846af5f0a8b
Faizan Akram <faizanakram1637@gmail.com>
2025-11-05 00:25:28 +0500

    remove logs

--- src/app/api/subscription/route.ts
+++ src/app/api/subscription/route.ts
@@ -7,12 +7,9 @@ import { handleError } from '@/lib/utils/error-handler';
 export async function GET(req: NextRequest) {
   try {
     const userId = await verifyAuth(req);
-    console.log("User id in backened subscription function is ",userId)
+    
     const subscriptions = await getUserSubscriptions(userId);
     const activeSubscription = await getUserActiveSubscription(userId);
-    
-    console.log("subscriptions of the User...",subscriptions)
-    console.log("Active subscriptions of the User...",activeSubscription)
 
     return createSuccessResponse({
       subscriptions,
