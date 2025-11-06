e8470f727de9647cb7cee9990a37c199078de636
Faizan Akram <faizanakram1637@gmail.com>
2025-11-06 15:27:14 +0500

    fixe bugs

--- src/app/api/admin/transactions/route.ts
+++ src/app/api/admin/transactions/route.ts
@@ -2,12 +2,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getPendingTransactions } from '@/repositries/transaction.repositories';
 import { verifyAuth } from '@/middleware/auth';
+import { getUser } from '@/services/backened/user.service';
 
 export async function GET(request: NextRequest) {
   try {
     const userId = await verifyAuth(request);
+
+    const user=await getUser(userId)
     // Optional: check if user is admin
-    // if (!user.role === 'admin') throw new Error('Unauthorized');
+    if (!(user.role === 'admin')) throw new Error('Unauthorized');
 
     const txns = await getPendingTransactions();
     return NextResponse.json({
--- src/app/api/packages/route.ts
+++ src/app/api/packages/route.ts
@@ -1,9 +1,8 @@
-import { NextRequest } from 'next/server';
 import { getAllPackages } from '@/services/backened/package.service';
 import { createSuccessResponse } from '@/lib/utils/api-response';
 import { handleError } from '@/lib/utils/error-handler';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const packages = await getAllPackages();
     return createSuccessResponse({ packages });
